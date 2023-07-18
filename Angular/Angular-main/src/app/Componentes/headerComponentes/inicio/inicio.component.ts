0cc6163a021f8598ecfa10526de88ed465f25344
aAleSJ <thealecuella@gmail.com>
2023-07-18 19:02:55 -0300

    ale

--- Angular/Angular-main/src/app/Componentes/headerComponentes/inicio/inicio.component.ts
+++ Angular/Angular-main/src/app/Componentes/headerComponentes/inicio/inicio.component.ts
@@ -13,8 +13,8 @@ export class InicioComponent {
       {titulo:'Monito 1',descripcion:'ale1',img:'https://http2.mlstatic.com/D_NQ_NP_829339-MLA52559264640_112022-O.webp',precio:'20.000'},
       {titulo:'Monito 2',descripcion:'ale2',img:'https://http2.mlstatic.com/D_NQ_NP_829339-MLA52559264640_112022-O.webp',precio:'40.000'},
       {titulo:'Monitor 3',descripcion:'ale3',img:'https://http2.mlstatic.com/D_NQ_NP_829339-MLA52559264640_112022-O.webp',precio:'60.000'},
-      {titulo:'Monitor 4',descripcion:'ale4',img:'https://http2.mlstatic.com/D_NQ_NP_829339-MLA52559264640_112022-O.webp',precio:'80.000'},
-      {titulo:'Monitor 5',descripcion:'ale5',img:'https://http2.mlstatic.com/D_NQ_NP_829339-MLA52559264640_112022-O.webp',precio:'100.000'}
+      {titulo:'Monitor 4',descripcion:'ale4',img:'https://http2.mlstatic.com/D_NQ_NP_829339-MLA52559264640_112022-O.webp',precio:'80.000'}
+      
     ]
   }
   RegistrarProductos(){
