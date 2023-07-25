acd78c8fd7e6586ef7db1288c728f789b73199a4
aAleSJ <thealecuella@gmail.com>
2023-07-24 22:22:19 -0300

    ale

--- Angular/Angular-main/src/app/Componentes/headerComponentes/contactos/contactos.component.ts
+++ Angular/Angular-main/src/app/Componentes/headerComponentes/contactos/contactos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Formulario2 } from 'src/app/models/modelsF2/formulario2';
 
 @Component({
   selector: 'app-contactos',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactos.component.css']
 })
 export class ContactosComponent {
+  formulario2:Formulario2
+  formularios:Array<Formulario2>
 
+  constructor(){
+    this.formulario2=new Formulario2();
+    this.formularios= new Array<Formulario2>();
+  }
+
+  registrar(){
+    console.log(this.formulario2)
+    this.formularios.push(this.formulario2)
+    this.formulario2 = new Formulario2()
+    
+
+  }
 }
